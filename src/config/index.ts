2c764fba7ca23800a6fc39360e6801b1db3fd6ae
Shahriar Ahmed <ourshahriar@gmail.com>
2023-10-11 17:28:51 +0600

    Missing

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,9 +3,6 @@ import path from "path";
 
 dotenv.config({ path: path.join(process.cwd(), ".env") });
 
-// user: authAdmin
-// password: rL9Hb0zv1USRnBUe
-
 const config = {
   port: process.env.PORT,
   DB_URI: process.env.DB_URI,
