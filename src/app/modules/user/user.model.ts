9efb4e55ddb7d9e4de0fd48307ec6da809cf46a8
Shahriar Ahmed <ourshahriar@gmail.com>
2023-10-12 08:05:36 +0600

    [DONE] create User Interface and Model

---
+++ src/app/modules/user/user.model.ts
@@ -0,0 +1,31 @@
+import { Model, Schema, model } from 'mongoose'
+import { IUser } from './user.interface'
+
+type UserModel = Model<IUser, object>
+
+const userSchema = new Schema<IUser>(
+  {
+    id: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    role: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
+)
+
+const User = model<IUser, UserModel>('User', userSchema)
+
+export default User
