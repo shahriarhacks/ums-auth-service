ff150157d5f0d3c7dfe7777edfcf2ec393470327
Shahriar Ahmed <ourshahriar@gmail.com>
2023-10-14 14:52:41 +0600

    [DONE] Setup Logger Winstone Successfully

--- src/server.ts
+++ src/server.ts
@@ -2,18 +2,19 @@ import mongoose from 'mongoose'
 import app from './app'
 import http from 'http'
 import config from './config/index'
+import { errorLog, log } from './shared/logger'
 
 const server = http.createServer(app)
 
 async function connectDB() {
   try {
     await mongoose.connect(config.DB_URI as string)
-    console.log(`DB Connected Successfully`)
+    log.info(`DB Connected Successfully`)
     server.listen(config.port, () => {
-      console.log(`Server Connected Successfully on port ${config.port}`)
+      log.info(`Server Connected Successfully on port ${config.port}`)
     })
   } catch (error) {
-    console.log(`DB connected Failed ${error}`)
+    errorLog.error(`DB connected Failed ${error}`)
   }
 }
 connectDB()
