5c6c651848f3511b3986a8ebe023354cb1900fa4
Shahriar Ahmed <ourshahriar@gmail.com>
2023-10-17 17:31:24 +0600

    [DONE] setup Unhandled rejection and expression and gracefully stop the server

--- src/app.ts
+++ src/app.ts
@@ -1,28 +1,32 @@
-import express, { Application, Request, Response } from 'express'
-import cors from 'cors'
-import userRouter from './app/modules/user/user.router'
-import globalErrorHandler from './app/middlewares/globalErrorHandler'
+import cors from "cors";
+import express, { Application, Request, Response } from "express";
+import globalErrorHandler from "./app/middlewares/globalErrorHandler";
+import userRouter from "./app/modules/user/user.router";
 
-const app: Application = express()
+const app: Application = express();
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-app.use(cors())
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 //Application router call
-app.use('/api/v1/users', userRouter)
+app.use("/api/v1/users", userRouter);
 
 // Global Error Handler
 
-app.use(globalErrorHandler)
+app.use(globalErrorHandler);
 
-app.get('/health', (_req: Request, res: Response) => {
+app.get("/health", (_req: Request, res: Response) => {
   res.status(200).json({
     statusCode: res.statusCode,
     request: true,
-    message: 'Success',
+    message: "Success",
     data: null,
-  })
-})
+  });
+});
 
-export default app
+// app.get('/', (req: Request, res: Response, next: NextFunction) => {
+//   Promise.reject(new Error('Unhandled Promise Rejection'));
+// });
+
+export default app;
